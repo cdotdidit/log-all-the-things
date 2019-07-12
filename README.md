c6736fd2b35d583aff1c7fb74169e2ac65f026e9
cash <cdotcomma@gmail.com>
2019-07-12 12:46:54 -0700

    commit of fully working files

--- README.md
+++ README.md
@@ -1,5 +1,5 @@
 Node101: "Log-All-The-Things"
 
-This project will log every server request to the console and to a file called log.csv using local host 3000.
+This project will log every server request to the console and to a custom file called log.csv using local host 3000.
 
 To run, clone the files locally and run npm test in your terminal.
--- server/app.js
+++ server/app.js
@@ -1,62 +1,57 @@
-const express = require('express');
-const fs = require('fs');
-const app = express();
+const express = require('express');// this is the actual express server
+const app =  express();//you have to call the express server
+const fs = require('fs'); //controls separately stored files as part of a multiuser system
+const path = require('path'); //provides utilities for working with file and directory paths
+const morgan = require('morgan')//this is a logger so it logs all the data
+const moment = require('moment'); //date library for parsing, validating, manipulating, and formatting dates
 
-app.use((req, res, next) => {
-// write your logging code here
-var userGetData = req.get("User-agent");
-var newUserData = '"' + userGetData + '"';
-dataArray.push(newUserData);
+let object =[];
 
-var DateTime = new Date();
-var formatTime = DateTime.toISOString();
-dataArray.push(formatTime);
+    app.use((req, _res, next) => {//sends out a request to get the parsed data
 
-var Method = req.method;
-dataArray.push(Method);
+        let Agent = req.headers['user-agent'];
+        let Time = moment().format();
+        let Method = req.method
+        let Resource = req._parsedUrl['path']; //path is inside of the brackets because it is the information already being parsed
+        let Version = `HTTP/${req.httpVersion}`; //template literal
+        let Status = "200";
+            console.log(Agent + "," + Time + "," + Method + "," + Resource + "," + Version + "," + Status);
+    
+    
+    var userInfo = "/n" + Agent + "," + Time + "," + Method + "," + Resource + "," + Version + "," + Status; //this put the information on my DOM into human readable data in order to see what is happening
 
-var Resource = req.url;
-dataArray.push(Resource);
+var userInfo = //this is parsing the data from above^^
+{
+    'Agent' : req.headers['user-agent'],
+    'Time' : moment().format(),
+    'Method' : req.method,
+    'Resource' : req._parsedUrl['path'],//path is inside of the brackets because it is the information already being parsed
+    'Version' : `HTTP/${req.httpVersion}`, //template literal
+    'Status' : "200",
+};
 
-var httpVer = req.httpVersion;
-dataArray.push("HTTP/" + httpVer);
-
-var status = res.statusCode;
-dataArray.push(status);
-
-let newArrayofStr = dataArray.join(',');
-let breakLineOnArr = newArrayofStr + "\n";
-
-console.log(newArrayofStr);
-
-fs.appendFile("/tmp/log.csv", breakLineOnArr, (err) => {
-    if (err) throw err;
-    console.log('The data has been appended to the log CSV file!');
-  });
-function emptyTheArray(){
-  dataArray.length = 0;
-}
-emptyTheArray();
-
+object.push(userInfo); //after it got the data from above, it pushed the parsed data into the array
+fs.appendFile(path.resolve(__dirname, 'log.csv'),userInfo, function(err){
+    if (err) throw err; // this is the callback function
+});
 next();
+    });
 
-});
 
-app.get('/', (req, res) => {
+
+
+
+app.get('/', (req, res) => { //this tells it to go out and get the data from the route
 // write your code to respond "ok" here
-res.status(200).send("ok");
+res.setHeader('Content-Type', 'text/html');
+    res.writeHead(200, {'Content-type': 'text/plain'});
+    res.end('ok');
 });
 
-app.get('/logs', (req, res) => {
+app.get('/logs', (req, res ) => {// this says to go to the log route and get the jSON data and respond with it 
 // write your code to return a json object containing the log data here
-const csvFilePath='/Users/admin/projects/node101-log-all-the-things/tmp/log.csv'
-const csv=require('csvtojson')
-csv()
-.fromFile(csvFilePath)
-.then((jsonObj)=>{
-    console.log(jsonObj);
-res.status(200).send(jsonObj);
-})
+res.json(object);//JSON stands for Javascript Object Notation (it is a key value pair always)(key:value)
+// }
 });
 
 module.exports = app;
